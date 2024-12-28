5625ff5d9b52505dafb9464c53a49ce5bd20e21f
iwamoto123 <103933392+iwamoto123@users.noreply.github.com>
2024-12-28 11:56:23 +0900

    reactの初期プロジェクトファイルを追加

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      <header className="App-header">
+        abc
+      </header>
+    </div>
+  );
+}
+
+export default App;
