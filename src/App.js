48007b6d5add4273a6fcba2021f17550d17484da
iwamoto123 <103933392+iwamoto123@users.noreply.github.com>
2025-01-11 00:55:44 +0900

    Tailwindでflexboxを作ってみた

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="bg-red-500">
-      <header className="App-header">
-        abc
-      </header>
+    <div className="flex bg-blue-200 p-4 justify-center items-start h-screen">
+      <div className="flex space-x-4">
+        <span className="bg-blue-400 text-white p-2 rounded">abc</span>
+        <span className="bg-blue-400 text-white p-2 rounded">def</span>
+        <span className="bg-blue-400 text-white p-2 rounded">ghi</span>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
