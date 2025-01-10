b52013d663e8d72e3d746f00b717724ea13e04ad
iwamoto123 <103933392+iwamoto123@users.noreply.github.com>
2025-01-11 01:04:16 +0900

    色を300にした

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="flex bg-blue-200 p-4 justify-center items-start h-screen">
       <div className="flex space-x-4">
-        <span className="bg-blue-400 text-white p-2 rounded">abc</span>
-        <span className="bg-blue-400 text-white p-2 rounded">def</span>
-        <span className="bg-blue-400 text-white p-2 rounded">ghi</span>
+        <span className="bg-blue-300 text-white p-2 rounded">abc</span>
+        <span className="bg-blue-300 text-white p-2 rounded">def</span>
+        <span className="bg-blue-300 text-white p-2 rounded">ghi</span>
       </div>
     </div>
   );
